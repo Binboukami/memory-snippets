8956b0c8fd00af3c880c5db5678d6b34cbc67d72
Binboukami <luiz.butkeivicz@gmail.com>
2024-10-30 14:04:35 -0300

    add ARENA_IMPL define flag for implementation

--- arena.h
+++ arena.h
@@ -5,7 +5,7 @@
 
 #ifndef ARENA_MX_SZ_BYTES
 	#define ARENA_MX_SZ_BYTES 1024 // ~1KB
-#endif
+#endif // ARENA_MX_SZ_BYTES
 
 typedef struct
 {
@@ -15,60 +15,69 @@ typedef struct
 
 } Arena;
 
-Arena* CreateArena(size_t size)
-{
-	if(size > ARENA_MX_SZ_BYTES)
-	{
-		fprintf(
-				stderr,
-				"Maximum arena size exceeded: Failed to allocate with size '%lu'(bytes)\n",
-				size);
+Arena* CreateArena(size_t size);
+void* ArenaAlloc(Arena* arena, size_t size);
+size_t ArenaRemainingSize(Arena* arena);
+size_t ArenaClear(Arena* arena);
 
-		return NULL;
+#ifdef ARENA_IMPL
+
+	Arena* CreateArena(size_t size)
+	{
+		if(size > ARENA_MX_SZ_BYTES)
+		{
+			fprintf(
+					stderr,
+					"Maximum arena size exceeded: Failed to allocate with size '%lu'(bytes)\n",
+					size);
+
+			return NULL;
+		}
+
+		// alloc enough space for the arena struct and its body
+		void* mem = malloc(sizeof(Arena) + size); 
+
+		memset(mem, 0, sizeof(Arena) + size);
+
+		void* data_offset = (char*)(mem) + sizeof(Arena);
+
+		Arena* arena = (Arena*)mem; // Set the arena struct memory block as the header
+		
+		arena->offset = 0;
+		arena->capacity = size;
+		arena->data = data_offset; // Set the remaining block as the data
+
+		return arena;
 	}
 
-	// alloc enough space for the arena struct and its body
-	void* mem = malloc(sizeof(Arena) + size); 
-
-	memset(mem, 0, sizeof(Arena) + size);
-
-	void* data_offset = (char*)(mem) + sizeof(Arena);
-
-	Arena* arena = (Arena*)mem; // Set the arena struct memory block as the header
-	
-	arena->offset = 0;
-	arena->capacity = size;
-	arena->data = data_offset; // Set the remaining block as the data
-
-	return arena;
-}
-
-void* ArenaAlloc(Arena* arena, size_t size)
-{
-	bool overflows = (arena->offset + size) > (arena->capacity);
-
-	if(overflows)
+	void* ArenaAlloc(Arena* arena, size_t size)
 	{
-		fprintf(stderr,
-				"Maximum arena size exceeded: Failed to allocate with size '%lu'(bytes)\n",
-				size);
+		bool overflows = (arena->offset + size) > (arena->capacity);
 
-		return NULL;
+		if(overflows)
+		{
+			fprintf(stderr,
+					"Maximum arena size exceeded: Failed to allocate with size '%lu'(bytes)\n",
+					size);
+
+			return NULL;
+		}
+
+		arena->offset += size;
+
+		return (char*)arena->data + arena->offset;
 	}
 
-	arena->offset += size;
+	size_t ArenaRemainingSize(Arena* arena)
+	{
+		return arena->capacity - arena->offset;
+	}
 
-	return (char*)arena->data + arena->offset;
-}
+	size_t ArenaClear(Arena* arena)
+	{
+		arena->offset = 0;
+		memset(arena->data, 0, arena->capacity);
+		return arena->capacity;
+	}
 
-size_t ArenaRemainingSize(Arena* arena)
-{
-	return arena->capacity - arena->offset;
-}
-
-size_t ArenaClear(Arena* arena)
-{
-	arena->offset = 0;
-	memset(arena->data, 0, arena->capacity);
-	return arena->capacity;
-}
+#endif //ARENA_IMPL
